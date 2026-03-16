32f18ceed6a1815a0d31f6159e05c097cade535e
Francisco Samily <francisco.samily1@gmail.com>
2026-03-16 08:56:42 -0300

    Adiciona novas práticas

---
+++ src/Introducao/Array.java
@@ -0,0 +1,12 @@
+package Introducao;
+
+public class Array {
+    public static void main(String[] args) {
+        String[] nomes = new String[3];
+        int[][] numeros = new int[3][];
+        numeros[0] = new int[] {1,2,3};
+        numeros[1] = new int[] {4,5,6};
+        numeros[2] = new int[] {7,8,9};
+
+    }
+}
--- src/exercicios/SenhaUser.java
+++ src/exercicios/SenhaUser.java
@@ -3,11 +3,22 @@ package exercicios;
 import java.util.Scanner;
 
 public class SenhaUser {
+    static boolean senhaValida(String senhaF, String senha) {
+        return senhaF.equals(senha);
+    }
+    static Scanner ler = new Scanner(System.in);
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String senha = "roblox123";
-        String senhaUser = sc.next();
 
-        System.out.println(senhaUser.equals(senha));
+        for( int i = 0; i < 3; i++ ) {
+            String senhaUser = sc.next();
+            if(senhaValida(senhaUser, senha)) {
+                System.out.println("Senha correta! Bem vindo");
+                break;
+            }
+            System.out.println("Senha incorreta!\n");
+        }
+        sc.close();
     }
 }
